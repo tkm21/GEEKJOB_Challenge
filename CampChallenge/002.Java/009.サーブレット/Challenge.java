344d202640dbf33f66179a82cbb1fcf3811a4de3
Takumi Watanabe <f.mst.ak.sea@gmail.com>
2017-05-11 18:08:21 +0900

    課題の提出

---
+++ CampChallenge/002.Java/009.サーブレット/Challenge.java
@@ -0,0 +1,43 @@
+
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+public class FortuneTelling extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String luckList[] = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
+        Random rand = new Random();
+        Integer index = rand.nextInt(luckList.length);
+
+        out.print("今日の運勢は" + luckList[index] + "です");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
